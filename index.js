dcdd8637264da54383bae2648bf0c60c82a2d9ff
Kiran Kumar Kandula <114183802+chinna0425@users.noreply.github.com>
2024-08-24 11:24:21 +0530

    index.js

--- index.js
+++ index.js
@@ -2,27 +2,45 @@ let inputData = document.getElementById("textDisplay");
 
 //Emptying The Screen
 function clearAllData() {
-  inputData.classList.remove("error-msg");
-  inputData.value = "";
+    inputData.classList.remove("error-msg");
+    inputData.value = "";
 }
 
 //Deleting OneByOne Element
 function removeOneByOne() {
-  let s = inputData.value;
-  inputData.value = s.slice(0, -1);
+    let s = inputData.value;
+    inputData.value = s.slice(0, -1);
 }
 
 //Assigning and Showing Value in Screen
 function performOperation(data) {
-  inputData.value += data;
+    console.log(data)
+    inputData.value += data;
 }
 
 //Performing Calculation
 function resultCheck() {
-  try {
-    inputData.value = eval(inputData.value);
-  } catch (error) {
-    inputData.value = "Error";
-    inputData.classList.add("error-msg");
-  }
+    try {
+        inputData.value = eval(inputData.value);
+    } catch (error) {
+        inputData.value = "Error";
+        inputData.classList.add("error-msg");
+    }
 }
+
+//using the window or keyboard operation perform
+window.addEventListener("keydown", function(event) {
+    let keyVal = event.key;
+    if (keyVal >= '0' && keyVal <= '9') {
+        performOperation(keyVal);
+    }else if(keyVal==="Enter"){
+        resultCheck();
+    }else if(keyVal==="Backspace"){
+        removeOneByOne();
+    }else if(keyVal==="+" || keyVal==="-" || keyVal==="*" || keyVal==="/" || keyVal===""){
+        performOperation(keyVal);
+    }else if(keyVal==="Delete"){
+        clearAllData();
+    }
+    
+})
